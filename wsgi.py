06952d299b359acab35783ae70b334421aab848c
richardtraindl <richardtraindl@gmail.com>
2016-02-18 20:34:51 +0100

    settings

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,8 @@
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ironman.settings")
 
+from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
-
-
 
