362d3ecb306e4a9abbc36068b7f8c4ab286bf02e
richardtraindl <richardtraindl@gmail.com>
2016-02-18 21:38:16 +0100

    settings

--- immanuel/urls.py
+++ immanuel/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', kate.site.urls),
+    url(r'^$', kate.views.index, name='index'),
     url(r'^kate/', include('kate.urls', namespace="kate")),
 ]
