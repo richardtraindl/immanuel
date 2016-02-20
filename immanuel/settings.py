ab76554ac9fee8860c57b62d00e024027fe2e503
richardtraindl <richardtraindl@gmail.com>
2016-02-20 12:12:25 +0100

    settings

--- immanuel/settings.py
+++ immanuel/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/static'
 
 STATIC_URL = '/static/'
 
