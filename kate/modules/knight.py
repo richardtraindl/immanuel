c2a727018e5faabdbeec528f291aed73607302b9
richardtraindl <richard.traindl@chello.at>
2016-03-08 15:27:29 +0100

    cleaning

--- kate/modules/knight.py
+++ kate/modules/knight.py
@@ -1,9 +1,5 @@
 from kate.models import Match
 
-DIRS = {
-    'valid' : 9,
-    'undefined' : 10 }
-
 
 STEP_2N1E_X = 1
 STEP_2N1E_Y = 2
@@ -23,7 +19,7 @@ STEP_2N1W_X = -1
 STEP_2N1W_Y = 2
 
 
-def direction(srcx, srcy, dstx, dsty):
+def kn_dir(srcx, srcy, dstx, dsty):
     step_x = dstx - srcx
     step_y = dsty - srcy
     if(step_x == STEP_2N1E_X and step_y == STEP_2N1E_Y):
@@ -48,22 +44,19 @@ def direction(srcx, srcy, dstx, dsty):
 
 
 def is_move_ok(match, srcx, srcy, dstx, dsty, piece):
-    knight_direction = direction(srcx, srcy, dstx, dsty)
-    if(knight_direction == DIRS['undefined']):
+    direction = kn_dir(srcx, srcy, dstx, dsty)
+    if(direction == DIRS['undefined']):
         return False
 
-    if(match.color_of_piece(piece) == match.COLORS['white']):
-        pinned = DIRS['undefined']
-        # fesselung = gib_weisse_figur_fesselung(_session->brett, _gzug->start_x, _gzug->start_y, _session->kw_feldnr_x, _session->kw_feldnr_y); \
-    else:
-        pinned = DIRS['undefined']
-        # fesselung = gib_schwarze_figur_fesselung(_session->brett, _gzug->start_x, _gzug->start_y, _session->ks_feldnr_x, _session->ks_feldnr_y); \
+    color = Match.color_of_piece(piece)
 
-    if(pinned != DIRS['undefined']):
+    pin_dir = rules.pin_dir(match, srcx, srcy)
+
+    if(pin_dir != DIRS['undefined']:
         return False
 
     field = match.readfield(dstx, dsty)
-    if(match.color_of_piece(field)== match.color_of_piece(piece)):
+    if(match.color_of_piece(field) == color):
         return False
 
     return True
