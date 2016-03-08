cfa5e1db97769f9e5d6f6cfc20eecf3355d47a07
richardtraindl <richard.traindl@chello.at>
2016-03-08 15:51:50 +0100

    cleaning

--- kate/modules/knight.py
+++ kate/modules/knight.py
@@ -1,4 +1,5 @@
 from kate.models import Match
+from kate.modules import values, rules
 
 
 STEP_2N1E_X = 1
@@ -23,36 +24,36 @@ def kn_dir(srcx, srcy, dstx, dsty):
     step_x = dstx - srcx
     step_y = dsty - srcy
     if(step_x == STEP_2N1E_X and step_y == STEP_2N1E_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_1N2E_X and step_y == STEP_1N2E_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_1S2E_X and step_y == STEP_1S2E_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_2S1E_X and step_y == STEP_2S1E_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_2S1W_X and step_y == STEP_2S1W_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_1S2W_X and step_y == STEP_1S2W_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_1N2W_X and step_y == STEP_1N2W_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     elif(step_x == STEP_2N1W_X and step_y == STEP_2N1W_Y):
-        return DIRS['valid']
+        return rules.DIRS['valid']
     else:
-        return DIRS['undefined']
+        return rules.DIRS['undefined']
 
 
 
 def is_move_ok(match, srcx, srcy, dstx, dsty, piece):
     direction = kn_dir(srcx, srcy, dstx, dsty)
-    if(direction == DIRS['undefined']):
+    if(direction == rules.DIRS['undefined']):
         return False
 
     color = Match.color_of_piece(piece)
 
     pin_dir = rules.pin_dir(match, srcx, srcy)
 
-    if(pin_dir != DIRS['undefined']:
+    if(pin_dir != rules.DIRS['undefined']:
         return False
 
     field = match.readfield(dstx, dsty)
