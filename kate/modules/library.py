4ad8bed2a47a75a3c07241042d6d79d43e34d51b
richardtraindl <richardtraindl@gmail.com>
2016-05-21 09:38:38 +0200

    seeds

--- kate/modules/library.py
+++ kate/modules/library.py
@@ -1,16 +1,21 @@
 from kate.models import OpeningMove
-
+from kate.fixtures import seed_e2e4_e7e5
 
 def populate_openings(move_list)
-       previous = None
-       for move in move_list:
-              omove = OpeningMove(previous, move[0], move[1], move[2])
-              try:
-                  omove.save()
-              except:
-                  omove = OpeningMove.objects.get(movecnt=move[0], src=move[1], dst=move[2])
-              if(omove == None):
-                  break
-              else:
-                  previous = omove
+        prevmove = None
+        for move in move_list:
+            if(omove.movecnt == 1):
+                prevmove = None
+            else:
+                while(previous.movecnt != omove.movecnt - 1):
+                    prevmove = prevmove.previous
 
+            openingmove = OpeningMove(prevmove, move[0], move[1], move[2])
+            try:
+                openingmove.save()
+            except:
+                print("populate_openings error")
+                break
+
+
+populate_openings(seed_e2e4_e7e5.M1)
