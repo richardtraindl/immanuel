a54a2298f3c2961632a1774f97e2b62ce7f2c295
richardtraindl <richardtraindl@gmail.com>
2017-03-10 20:48:10 +0100

    changes

--- immanuel/settings.py
+++ immanuel/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticdir'),
 ]
 
 STATIC_URL = '/static/'
