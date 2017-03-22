0ce05a2787b590846ca37e1f5e491d7b20ef35b0
Richard Traindl <richard.traindl@chello.at>
2017-03-22 17:18:24 +0100

    Update rook.py

--- kate/modules/rook.py
+++ kate/modules/rook.py
@@ -126,6 +126,31 @@ def does_support_attacked(match, srcx, srcy):
     return False
 
 
+def score_supports_of_attacked(match, srcx, srcy):
+    score = 0
+
+    rook = match.readfield(srcx, srcy)
+
+    if(rook != Match.PIECES['wRk'] and rook != Match.PIECES['wQu'] and rook != Match.PIECES['bRk'] and rook != Match.PIECES['bQu']):
+        return score
+
+    color = Match.color_of_piece(bishop)
+
+    for i in range(4):
+        stepx = STEPS[i][0]
+        stepy = STEPS[i][1]
+        x1, y1 = rules.search(match, srcx, srcy, stepx , stepy)
+        if(x1 != rules.UNDEF_X):
+            piece = match.readfield(x1, y1)
+            if(piece == Match.PIECES['blk'] or piece == Match.PIECES['wKg'] or piece == Match.PIECES['bKg']):
+                continue
+            if( color == Match.color_of_piece(piece) ):
+                if(rules.is_field_attacked(match, Match.REVERSED_COLORS[color], x1, y1)):
+                    score += Match.SCORES[piece]
+
+    return score 
+
+  
 def rk_dir(srcx, srcy, dstx, dsty):
     DIRS = rules.DIRS
     if( (srcx == dstx) and (srcy < dsty) ):
