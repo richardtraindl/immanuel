0cd4e522a07a3f5826c0283a572031a2a979c0b4
Richard Traindl <richard.traindl@chello.at>
2017-03-24 14:01:43 +0100

    Update generic.py

--- kate/modules/generic.py
+++ kate/modules/generic.py
@@ -3,26 +3,31 @@ from kate.modules import rules
             
             
 def generic.do_move(match, move, srcpiece, dstpiece):
-            self.writefield(srcx, srcy, Match.PIECES['blk'])
-            self.writefield(dstx, dsty, srcpiece)
-            if(dstpiece != Match.PIECES['blk']):
-                self.fifty_moves_count = 0
-            else:
-                self.fifty_moves_count += 1
+    move.move_type = Move.TYPES['standard']
+    move.captured_piece = dstpiece
 
-            if(srcpiece == Match.PIECES['wRk']):
-                if(srcx == 0 and srcy == 0 and self.wRk_a1_first_movecnt == 0):
-                    self.wRk_a1_first_movecnt = self.count
-                elif(srcx == 7 and srcy == 0 and self.wRk_h1_first_movecnt == 0):
-                    self.wRk_h1_first_movecnt = self.count
-            elif(srcpiece == Match.PIECES['bRk']):
-                if(srcx == 0 and srcy == 7 and self.bRk_a8_first_movecnt == 0):
-                    self.bRk_a8_first_movecnt = self.count
-                elif(srcx == 7 and srcy == 7 and self.bRk_h8_first_movecnt == 0):
-                    self.bRk_h8_first_movecnt = self.count
-            move.fifty_moves_count = self.fifty_moves_count
-            move.move_type = Move.TYPES['standard']
-            move.captured_piece = dstpiece
-            self.score += Match.SCORES[dstpiece]
-            self.move_list.append(move)
-            return move
+    match.writefield(move.srcx, move.srcy, Match.PIECES['blk'])
+    match.writefield(move.dstx, move.dsty, srcpiece)
+    if(dstpiece != Match.PIECES['blk']):
+        match.fifty_moves_count = 0
+        move.fifty_moves_count = match.fifty_moves_count
+    else:
+        match.fifty_moves_count += 1
+        move.fifty_moves_count = match.fifty_moves_count
+
+    if(srcpiece == Match.PIECES['wRk']):
+        if(move.srcx == 0 and move.srcy == 0 and match.wRk_a1_first_movecnt == 0):
+            match.wRk_a1_first_movecnt = match.count
+        elif(move.srcx == 7 and move.srcy == 0 and match.wRk_h1_first_movecnt == 0):
+            match.wRk_h1_first_movecnt = match.count
+    elif(srcpiece == Match.PIECES['bRk']):
+        if(move.srcx == 0 and move.srcy == 7 and match.bRk_a8_first_movecnt == 0):
+            match.bRk_a8_first_movecnt = match.count
+        elif(move.srcx == 7 and move.srcy == 7 and match.bRk_h8_first_movecnt == 0):
+            match.bRk_h8_first_movecnt = match.count
+
+    match.score += Match.SCORES[dstpiece]
+    match.move_list.append(move)
+
+    return move
+
