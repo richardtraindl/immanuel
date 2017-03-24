4b17d12d2f0d1dca7196aafde8f28890020be73a
Richard Traindl <richard.traindl@chello.at>
2017-03-24 14:51:32 +0100

    Update generic.py

--- kate/modules/generic.py
+++ kate/modules/generic.py
@@ -32,3 +32,34 @@ def generic.do_move(match, move, srcpiece, dstpiece):
 
     return move
 
+
+def generic.undo_move(match, move):
+    piece = self.readfield(move.dstx, move.dsty)
+    self.writefield(move.srcx, move.srcy, piece)
+    self.writefield(move.dstx, move.dsty, move.captured_piece)
+    self.score -= self.SCORES[move.captured_piece]
+    if(piece == Match.PIECES['wKg']):
+        self.wKg_x = move.srcx
+        self.wKg_y = move.srcy
+        if(self.wKg_first_movecnt == self.count + 1):
+            self.wKg_first_movecnt = 0
+    elif(piece == Match.PIECES['bKg']):
+        self.bKg_x = move.srcx
+        self.bKg_y = move.srcy
+        if(self.bKg_first_movecnt == self.count + 1):
+            self.bKg_first_movecnt = 0
+    elif(piece == Match.PIECES['wRk']):
+        if(self.wRk_a1_first_movecnt == self.count + 1):
+            self.wRk_a1_first_movecnt = 0
+        elif(self.wRk_h1_first_movecnt == self.count + 1):
+            self.wRk_h1_first_movecnt = 0
+    elif(piece == Match.PIECES['bRk']):
+        if(self.bRk_a8_first_movecnt == self.count + 1):
+            self.bRk_a8_first_movecnt = 0
+        elif(self.bRk_h8_first_movecnt == self.count + 1):
+            self.bRk_h8_first_movecnt = 0
+
+    return move
+
+
+
