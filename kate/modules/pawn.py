526ad59710571d703ebbebe5f496ab45af25ce0f
richardtraindl <richardtraindl@gmail.com>
2017-04-08 08:56:08 +0200

    fixes

--- kate/modules/pawn.py
+++ kate/modules/pawn.py
@@ -192,7 +192,7 @@ def score_supports_of_attacked(match, srcx, srcy):
                 continue
             if( color == Match.color_of_piece(piece) ):
                 if(rules.is_field_attacked(match, Match.REVERSED_COLORS[color], x1, y1)):
-                    score -= Match.SUPPORTED_SCORES[piece]
+                    score += Match.SUPPORTED_SCORES[piece]
 
     return score
 
