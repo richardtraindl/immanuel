de50586f6828f786916de36d28d571f5f11af2be
Richard Traindl <richardtraindl@gmail.com>
2017-05-02 07:46:36 +0200

    Update rook.py

--- kate/modules/rook.py
+++ kate/modules/rook.py
@@ -110,7 +110,10 @@ def score_attacks(match, srcx, srcy):
                     pin_dir = rules.pin_dir(match, x1, y1)
                     direction = rk_dir(srcx, srcy, x1, y1)
                     if(pin_dir == direction):
-                        score += Match.ATTACKED_SCORES[piece] // 2
+                        if(piece == Match.PIECES['wrk'] or piece == Match.PIECES['brk'] or piece == Match.PIECES['wqu'] or piece == Match.PIECES['bqu']):
+                            score += Match.ATTACKED_SCORES[piece] // 4
+                        else:
+                            score += Match.ATTACKED_SCORES[piece] // 2
     return score
 
 
