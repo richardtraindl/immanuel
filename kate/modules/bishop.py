425242b466f45468824518be08b2d106ebb3760c
Richard Traindl <richardtraindl@gmail.com>
2017-05-04 14:36:44 +0200

    Update bishop.py

--- kate/modules/bishop.py
+++ kate/modules/bishop.py
@@ -56,14 +56,13 @@ def does_attack(match, srcx, srcy, dstx, dsty):
                     return True, 1 # priority
                 else:                  
                     pin_dir = rules.pin_dir(match, x1, y1)
-                    direction = bp_dir(srcx, srcy, x1, y1)
-                    if(pin_dir == direction):
+                    if(pin_dir == rules.DIRS['undefined']):
                         return True, 1 # priority
                     else:
-                        if(piece == Match.PIECES['wPw'] or piece == Match.PIECES['bPw']):
-                            priority = min(priority, 3)
-                        else:
+                        if(Match.PIECES_RANK[bishop] <= Match.PIECES_RANK[piece]):
                             priority = min(priority, 2)
+                        else:
+                            priority = min(priority, 3)
 
     if(priority == 5):
         return False, 0
