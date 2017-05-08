7ab51556b29e3570f4df7d4ee5ceb6df3cfab285
Richard Traindl <richardtraindl@gmail.com>
2017-05-08 09:04:34 +0200

    Update bishop.py

--- kate/modules/bishop.py
+++ kate/modules/bishop.py
@@ -34,25 +34,21 @@ def is_field_touched(match, color, fieldx, fieldy):
     return False
 
 
-def is_field_touched_ext(match, color, fieldx, fieldy):
-    piece = Match.PIECES['blk']
+def list_field_touches(match, color, fieldx, fieldy):
+    touches = []
     
     for i in range(4):
         stepx = STEPS[i][0]
         stepy = STEPS[i][1]
         x1, y1 = rules.search(match, fieldx, fieldy, stepx, stepy)
         if(x1 != rules.UNDEF_X):
-            newpiece = match.readfield(x1, y1)
-            if( (color == Match.COLORS['white'] and (newpiece == Match.PIECES['wQu'] or newpiece == Match.PIECES['wBp'])) or
-                (color == Match.COLORS['black'] and (newpiece == Match.PIECES['bQu'] or newpiece == Match.PIECES['bBp'])) ):
-                if(Match.PIECES_RANK[newpiece] < Match.PIECES_RANK[piece]):
-                    piece = newpiece
+            piece = match.readfield(x1, y1)
+            if( (color == Match.COLORS['white'] and (piece == Match.PIECES['wQu'] or piece == Match.PIECES['wBp'])) or
+                (color == Match.COLORS['black'] and (piece == Match.PIECES['bQu'] or piece == Match.PIECES['bBp'])) ):
+                touches.append([piece, x1, y1])
 
-    if(piece == Match.PIECES['blk']):
-        return False, Match.PIECES['blk']
-    else:
-        return True, piece
- 
+    return touches
+
   
 def does_attack(match, srcx, srcy, dstx, dsty):
     priority = 5
