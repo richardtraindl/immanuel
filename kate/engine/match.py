6768686980e98d4a8edb71001fd6b5b98b082cca
Richard Traindl <richardtraindl@gmail.com>
2017-05-09 16:28:16 +0200

    Update match.py

--- kate/engine/match.py
+++ kate/engine/match.py
@@ -1,23 +1,48 @@
-status = models.PositiveSmallIntegerField(null=False, default=STATUS['open'])
-    count = models.SmallIntegerField(null=False, default=0)
-    score = models.IntegerField(null=False, default=0)
-    begin = models.DateTimeField(default=timezone.now)
-    white_player = models.CharField(max_length=100, blank=False)
-    white_player_human = models.BooleanField(null=False, default=True)
-    elapsed_time_white = models.IntegerField(null=False, default=0)
-    black_player = models.CharField(max_length=100, blank=False)
-    black_player_human = models.BooleanField(null=False, default=True)
-    elapsed_time_black = models.IntegerField(null=False, default=0)
-    level = models.SmallIntegerField(null=False, default=1)
-    board = ArrayField(ArrayField(models.PositiveSmallIntegerField(null=False, blank=False, default=PIECES['blk']), size=8), size=8)
-    fifty_moves_count = models.SmallIntegerField(null=False, default=0)
-    wKg_x = models.SmallIntegerField(null=False, default=0)
-    wKg_y = models.SmallIntegerField(null=False, default=0)
-    bKg_x = models.SmallIntegerField(null=False, default=0)
-    bKg_y = models.SmallIntegerField(null=False, default=0)
-    wKg_first_movecnt = models.SmallIntegerField(null=False, default=0)
-    bKg_first_movecnt = models.SmallIntegerField(null=False, default=0)
-    wRk_a1_first_movecnt = models.SmallIntegerField(null=False, default=0)
-    wRk_h1_first_movecnt = models.SmallIntegerField(null=False, default=0)
-    bRk_a8_first_movecnt = models.SmallIntegerField(null=False, default=0)
-    bRk_h8_first_movecnt = models.SmallIntegerField(null=False, default=0)
+
+
+
+STATUS = {
+            'open' : 1,
+            'draw' : 2,
+            'winner_white' : 3,
+            'winner_black' : 4,
+            'cancelled' : 5 }
+
+LEVELS = {
+            'blitz' : 0,
+            'low' : 1,
+            'medium' : 2,
+            'high' : 3 }
+
+E1_X = 3
+E1_Y = 0
+E8_X = 3
+E8_Y = 7
+
+class Match:
+    # static_elem = 123
+
+    def __init__(self):
+        self.status = STATUS['open']
+        self.count = 0
+        self.score = 0
+        self.white_player = ""
+        self.white_player_human = True
+        self.elapsed_time_white = 0
+        self.black_player = ""
+        self.black_player_human = True
+        self.elapsed_time_black = 0
+        self.level = LEVELS['blitz']
+        self.board = []
+        self.fifty_moves_count = 0
+        self.wKg_x = E1_X
+        self.wKg_y = E1_Y
+        self.bKg_x = E8_X
+        self.bKg_y = E8_Y
+        self.wKg_first_movecnt = 0
+        self.bKg_first_movecnt = 0
+        self.wRk_a1_first_movecnt = 0
+        self.wRk_h1_first_movecnt = 0
+        self.bRk_a8_first_movecnt = 0
+        self.bRk_h8_first_movecnt = 0
+
