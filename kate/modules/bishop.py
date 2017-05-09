8a3d06b08af56670e0299a5b0c8dc634a98af61b
Richard Traindl <richardtraindl@gmail.com>
2017-05-09 11:04:00 +0200

    Update bishop.py

--- kate/modules/bishop.py
+++ kate/modules/bishop.py
@@ -75,9 +75,9 @@ def does_attack(match, srcx, srcy, dstx, dsty):
                     if(pin_dir != rules.DIRS['undefined']):
                         priority = min(priority, 2)
                     else:
-                        match.writefield(move.srcx, move.srcy, Match.PIECES['blk'])
+                        match.writefield(srcx, srcy, Match.PIECES['blk'])
                         touched = rules.is_field_touched(match, opp_color, dstx, dsty)
-                        match.writefield(move.srcx, move.srcy, bishop)
+                        match.writefield(srcx, srcy, bishop)
                         if(touched):
                             priority = min(priority, 3)
                         elif(rules.is_field_touched(match, opp_color, x1, y1)):
