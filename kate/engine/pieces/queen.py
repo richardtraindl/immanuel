87b80a75650e8b026e38bb9624ae7857d7bf1a7a
Richard Traindl <richardtraindl@gmail.com>
2017-05-10 16:18:26 +0200

    Update and rename kate/engine/queen.py to kate/engine/pieces/queen.py

--- kate/engine/pieces/queen.py
+++ kate/engine/pieces/queen.py
@@ -1,8 +1,8 @@
-from kate.models import Match
-from kate.modules import rules, rook, bishop
+from kate.engine.match.pieces import rook, bishop
+from kate.engine.match import *
 
 
-blank = Match.PIECES['blk']
+blank = PIECES['blk']
 GEN_STEPS = [ [[0, 1, blank], [0, 2, blank], [0, 3, blank], [0, 4, blank], [0, 5, blank], [0, 6, blank], [0, 7, blank]],
               [[0, -1, blank], [0, -2, blank], [0, -3, blank], [0, -4, blank], [0, -5, blank], [0, -6, blank], [0, -7, blank]],
               [[1, 0, blank], [2, 0, blank], [3, 0, blank], [4, 0, blank], [5, 0, blank], [6, 0, blank], [7, 0, blank]],
