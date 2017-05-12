ba4e4f61700020fd21a4e95000762be06bdcc92f
Richard Traindl <richardtraindl@gmail.com>
2017-05-12 12:55:06 +0200

    Update views.py

--- kate/views.py
+++ kate/views.py
@@ -5,7 +5,6 @@ from django.template import RequestContext
 from kate.models import Match as ModelMatch, Move as ModelMove, Comment as ModelComment
 from kate.engine.match import *
 from kate.engine.move import *
-from kate.engine.matchmove import *
 from kate.modules import interface
 from kate.engine import rules
 from kate.utils import *
