8dd27f2cb0ceee162846d6361151a71eeeb4bc42
Richard Traindl <richardtraindl@gmail.com>
2017-05-17 16:09:37 +0200

    Update forms.py

--- kate/forms.py
+++ kate/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
 
-classDoMoveForm(forms.Form):
-    move_src = forms.CharField(label='move-src', id='move-src' max_length=3)
-    move_dst = forms.CharField(label='move-dst', id='move-dst' max_length=3)
-    prom_piece = forms.CharField(label='prom-piece', id='prom-piece' max_length=3)
-    switch = forms.CharField(label='switch', id='switch' max_length=3)
+class DoMoveForm(forms.Form):
+    move_src = forms.CharField(widget=forms.TextInput(attrs={'id':'move-src'}), label='move-src', id='move-src' max_length=3)
+    move_dst = forms.CharField(widget=forms.TextInput(attrs={'id':'move-dst'}), label='move-dst', id='move-dst' max_length=3)
+    prom_piece = forms.CharField(widget=forms.TextInput(attrs={'id':'prom-piece'}), label='prom-piece', id='prom-piece' max_length=3)
+    switch = forms.IntegerField(widget=forms.TextInput(attrs={'id':'switch'}), label='switch', id='switch')
 
    
