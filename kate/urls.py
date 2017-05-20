d8163ea9f36315a5c5fc7d29b43b6376a711ded7
richardtraindl <richardtraindl@gmail.com>
2017-05-20 09:09:05 +0200

    url

--- kate/urls.py
+++ kate/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^(?P<matchid>[0-9]+)/match/$', views.match, name='match'),
     url(r'^(?P<matchid>[0-9]+)/(?P<switch>[0-1])/match/$', views.match, name='match'),
     url(r'^(?P<matchid>[0-9]+)/(?P<switch>[0-1])/(?P<msg>[0-9]+)/match/$', views.match, name='match'),
     url(r'^(?P<matchid>[0-9]+)/(?P<switch>[0-1])/domove/$', views.do_move, name='domove'),
