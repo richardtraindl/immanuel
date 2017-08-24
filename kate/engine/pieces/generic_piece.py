f5569351fab4b44fb4909d295388210e21adf8bf
Richard Traindl <richardtraindl@gmail.com>
2017-08-24 10:56:50 +0200

    Update generic_piece.py

--- kate/engine/pieces/generic_piece.py
+++ kate/engine/pieces/generic_piece.py
@@ -18,24 +18,49 @@ def count_contacts(contacts):
     return pawncnt, officercnt, queencnt
 
 
-def contacts_to_token(frdlycontacts, enmycontacts):
+def contacts_to_token(frdlycontacts, enmycontacts, mode):
     token = 0x0
+    
+    if(mode == "FIELDTOUCHES"):
+        pawncnt, officercnt, queencnt = count_contacts(frdlycontacts)
+        if(pawncnt > 0):
+            token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_PAWN
+        if(officercnt > 0):
+            token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_OFFICER
+        if(queencnt > 0):
+            token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_QUEEN
 
-    pawncnt, officercnt, queencnt = count_contacts(frdlycontacts)
-    if(pawncnt > 0):
-        token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_PAWN
-    if(officercnt > 0):
-        token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_OFFICER
-    if(queencnt > 0):
-        token = token | MV_DSTFIELD_IS_FRDLYTOUCHED_BY_QUEEN
+        pawncnt, officercnt, queencnt = count_contacts(enmycontacts)
+        if(pawncnt > 0):
+            token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_PAWN
+        if(officercnt > 0):
+            token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_OFFICER
+        if(queencnt > 0):
+            token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_QUEEN
+    elif(mode == "ATTACKTOUCHES"):
+        pawncnt, officercnt = count_contacts(frdlycontacts)
+        if(pawncnt > 0):
+            token = token | ATTACKED_IS_ADD_ATT_FROM_PAWN
+        if(officercnt > 0):
+            token = token | ATTACKED_IS_ADD_ATT_FROM_OFFICER
 
-    pawncnt, officercnt, queencnt = count_contacts(enmycontacts)
-    if(pawncnt > 0):
-        token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_PAWN
-    if(officercnt > 0):
-        token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_OFFICER
-    if(queencnt > 0):
-        token = token | MV_DSTFIELD_IS_ENMYTOUCHED_BY_QUEEN
+        pawncnt, officercnt = count_contacts(enmycontacts)
+        if(pawncnt > 0):
+            token = token | ATTACKED_IS_SUPP_BY_PAWN
+        if(officercnt > 0):
+            token = token | ATTACKED_IS_SUPP_BY_OFFICER
+    elif(mode == "SUPPORTTOUCHES"):
+        pawncnt, officercnt = count_contacts(frdlycontacts)
+        if(pawncnt > 0):
+            token = token | SUPPORTED_IS_ADD_SUPP_BY_PAWN
+        if(officercnt > 0):
+            token = token | SUPPORTED_IS_ADD_SUPP_BY_OFFICER
+
+        pawncnt, officercnt = count_contacts(enmycontacts)
+        if(pawncnt > 0):
+            token = token | SUPPORTED_IS_ATT_FROM_PAWN
+        if(officercnt > 0):
+            token = token | SUPPORTED_IS_ATT_FROM_OFFICER
 
     return token
 
