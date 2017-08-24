53d8105ae10bdffea1e16559ffab55936eae5acd
Richard Traindl <richardtraindl@gmail.com>
2017-08-24 10:52:35 +0200

    Update bishop.py

--- kate/engine/pieces/bishop.py
+++ kate/engine/pieces/bishop.py
@@ -134,7 +134,7 @@ def touches(match, srcx, srcy, dstx, dsty):
 
     match.writefield(srcx, srcy, bishop)
 
-    token = token | contacts_to_token(frdlycontacts, enmycontacts)
+    token = token | contacts_to_token(frdlycontacts, enmycontacts, "FIELDTOUCHES")
     ###
 
     for i in range(4):
@@ -162,17 +162,7 @@ def touches(match, srcx, srcy, dstx, dsty):
 
                 match.writefield(srcx, srcy, bishop)
 
-                pawncnt, officercnt = count_contacts(frdlycontacts)
-                if(pawncnt > 0):
-                    token = token | ATTACKED_IS_ADD_ATT_FROM_PAWN
-                if(officercnt > 0):
-                    token = token | ATTACKED_IS_ADD_ATT_FROM_OFFICER
-
-                pawncnt, officercnt = count_contacts(enmycontacts)
-                if(pawncnt > 0):
-                    token = token | ATTACKED_IS_SUPP_BY_PAWN
-                if(officercnt > 0):
-                    token = token | ATTACKED_IS_SUPP_BY_OFFICER
+                token = token | contacts_to_token(frdlycontacts, enmycontacts, "ATTACKTOUCHES")
                 ###
             else:
                 if(x1 == srcx and y1 == srcy):
@@ -196,17 +186,7 @@ def touches(match, srcx, srcy, dstx, dsty):
 
                 match.writefield(srcx, srcy, bishop)
 
-                pawncnt, officercnt = count_contacts(frdlycontacts)
-                if(pawncnt > 0):
-                    token = token | SUPPORTED_IS_ADD_SUPP_BY_PAWN
-                if(officercnt > 0):
-                    token = token | SUPPORTED_IS_ADD_SUPP_BY_OFFICER
-
-                pawncnt, officercnt = count_contacts(enmycontacts)
-                if(pawncnt > 0):
-                    token = token | SUPPORTED_IS_ATT_FROM_PAWN
-                if(officercnt > 0):
-                    token = token | SUPPORTED_IS_ATT_FROM_OFFICER
+                token = token | contacts_to_token(frdlycontacts, enmycontacts, "SUPPORTTOUCHES")
                 ###
 
     return token
