250bed420b34ae57e52b03d1f2b0dd538a2d68a7
Richard Traindl <richardtraindl@gmail.com>
2017-12-15 19:08:58 +0100

    Update analyze_position.py

--- kate/engine/analyze_position.py
+++ kate/engine/analyze_position.py
@@ -271,12 +271,12 @@ def score_position(match, movecnt):
     else:
         value = match.score
 
-        # value += score_contacts(match, COLORS['white'])
+        value += score_contacts(match, COLORS['white'])
 
-        # value += score_contacts(match, COLORS['black'])
+        value += score_contacts(match, COLORS['black'])
 
-        #if(match.count < 30):
-            # value += score_opening(match)
+        if(match.count < 30):
+            value += score_opening(match)
 
         if(match.count >= 30):
             value += score_endgame(match)
