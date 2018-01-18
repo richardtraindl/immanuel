76349308d31b05083c798f7a1e9e224c6012ee72
Richard Traindl <richardtraindl@gmail.com>
2018-01-18 17:25:02 +0100

    Update bishop.py

--- kate/engine/pieces/bishop.py
+++ kate/engine/pieces/bishop.py
@@ -23,17 +23,22 @@ GEN_STEPS = [ [[1, 1, blank], [2, 2, blank], [3, 3, blank], [4, 4, blank], [5, 5
               [[-1, 1, blank], [-2, 2, blank], [-3, 3, blank], [-4, 4, blank], [-5, 5, blank], [-6, 6, blank], [-7, 7, blank]] ]
 
 
-def is_field_touched(match, color, fieldx, fieldy):
+def is_field_touched(match, fieldx, fieldy):
+    whitetouch = False
+    blacktouch = False
+
     for i in range(4):
         stepx = STEPS[i][0]
         stepy = STEPS[i][1]
         x1, y1 = rules.search(match, fieldx, fieldy, stepx, stepy)
         if(x1 != rules.UNDEF_X):
             piece = match.readfield(x1, y1)
-            if( (color == COLORS['white'] and piece == PIECES['wBp']) or
-                (color == COLORS['black'] and piece == PIECES['bBp']) ):
-                return True
-    return False
+            if(piece == PIECES['wBp']):
+                whitetouch = True
+            elif(piece == PIECES['bBp']):
+                blacktouch = True
+
+    return whitetouch, blacktouch
 
 
 def is_move_stuck(match, srcx, srcy, dstx, dsty):
