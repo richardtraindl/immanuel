3a7f82655daebb8fb1d809179491afc656b5f0fe
richardtraindl <richardtraindl@gmail.com>
2018-01-30 21:09:48 +0100

    fix

--- kate/engine/calc.py
+++ kate/engine/calc.py
@@ -221,7 +221,7 @@ def select_maxcnt(match, depth, priomoves, priocnts, last_priomove):
         max_dpth = 9
 
     if(depth <= dpth):
-        return min(cnt, mvcnt)
+        return max(cnt, high_prio_mvcnt)
     # elif(match.level == LEVELS['blitz'] and depth <= dpth + 2 and is_stormy(match)):
     #    return high_prio_mvcnt + 1
     elif(depth <= max_dpth and is_last_move_stormy(last_prio, last_token) and is_stormy(match)):
