b6a68b37863379e4a4a34cfbb08f7798532d7a09
Richard Traindl <richardtraindl@gmail.com>
2018-03-02 21:58:28 +0100

    Update urls.py

--- kate/urls.py
+++ kate/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^index.html$', views.index, name='index'),
     url(r'^match/(?P<matchid>[0-9]+)/$', views.match, name='match'),
     url(r'^domove/(?P<matchid>[0-9]+)/$', views.do_move, name='domove'),
     url(r'^undomove/(?P<matchid>[0-9]+)/$', views.undo_move, name='undomove'),
