b6d25bb05784da691eac0df94211406b59b7f9ac
Richard Traindl <richardtraindl@gmail.com>
2018-04-05 11:23:24 +0200

    Update analyze_position.py

--- kate/engine/analyze_position.py
+++ kate/engine/analyze_position.py
@@ -500,7 +500,7 @@ def is_stormy(match):
 
             frdlytouches, enmytouches = field_touches(match, piece_color, x, y)
 
-            if(is_pinned(match, x, y)[0] or is_soft_pin(match, x, y)):
+            if(rules.is_pinned(match, x, y)[0] or is_soft_pin(match, x, y)):
                 if(color == piece_color):
                     if(len(frdlytouches) < len(enmytouches)):
                         return True
