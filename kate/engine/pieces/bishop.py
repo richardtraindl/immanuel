0349b17b9d899ff38020e4f53099ab6c6770e568
Richard Traindl <richardtraindl@gmail.com>
2018-04-30 13:50:06 +0200

    Update bishop.py

--- kate/engine/pieces/bishop.py
+++ kate/engine/pieces/bishop.py
@@ -193,7 +193,8 @@ def disclosures(match, color, excluded_dir, srcx, srcy, discl_attacked, discl_su
         for i in range(0, 2, 1):
             stepx = STEPS[j+i][0]
             stepy = STEPS[j+i][1]
-            if(excluded_dir == bp_dir(srcx, srcy, (srcx + stepx), (srcy + stepy))):
+            direction == bp_dir(srcx, srcy, (srcx + stepx), (srcy + stepy))
+            if(direction == excluded_dir or direction == REVERSE_DIRS[excluded_dir]):
                 break
             x1, y1 = rules.search(match, srcx, srcy, stepx, stepy)
             if(x1 != rules.UNDEF_X):
