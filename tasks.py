be4fcf6be7c2b281a48af062855802ff1f8e493f
richardtraindl <richardtraindl@gmail.com>
2018-06-03 19:41:08 +0200

    changes

--- tasks.py
+++ tasks.py
@@ -4,14 +4,14 @@ from kate.engine.calc import calc_move, SearchComm
 from kate.modules import interface
 
 
-@job("high") # , timeout=600 timeout is optional
+#@job("high")
+@job("default")
 def job_calc_and_do_move(modelmatch, match):
     job = get_current_job()
     job.meta['matchid'] = match.id
     job.meta['currentsearch'] = ""
     job.meta['terminate'] = 0
     job.save_meta()
-    #job.save()
 
     searchcomm = SearchComm()
     fetchcandidates = interface.FetchCandidatesThread(match.id, searchcomm)
