506e85bc0343741bf439a0deb198493a0f39c457
Richard Traindl <richardtraindl@gmail.com>
2018-08-21 16:31:27 +0200

    Update knight.py

--- kate/engine/pieces/knight.py
+++ kate/engine/pieces/knight.py
@@ -110,8 +110,29 @@ class cKnight(cPiece):
                     self.match.writefield(self.xpos, self.ypos, self.piece)
                     ###
 
-    def is_move_disclosure(self, dstx, dsty, discl_attacked, discl_supported):
+    def move_defends_forked_field(self, dstx, dsty):
+        from .. analyze_helper import is_field_forked
+
+        for step in slef.STEPS:
+            x1 = dstx + step[0]
+            y1 = dsty + step[1]
+
+            if(x1 == self.xpos and y1 == self.ypos):
+                continue
+
+            if(self.match.is_inbounds(x1, y1)):
+                piece = self.match.readfield(x1, y1)
+
+                if(self.match.color_of_piece(piece) == self.color):
+                    if(is_field_forked(self.match, piece, x1, y1)):
+                        #cfork = cFork(srcx, srcy, dstx, dsty, x1, y1)
+                        #analyses.lst_fork_defended.append(cfork)
+                        return True
+
         return False
 
+    def move_controles_file(self, dstx, dsty)
+        return False
+    
 # class end
 
