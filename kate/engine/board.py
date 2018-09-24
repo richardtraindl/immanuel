188d52eaad71b918a6e463bf107f83d3f9ebed9f
Richard Traindl <richardtraindl@gmail.com>
2018-09-24 15:19:20 +0200

    Update board.py

--- kate/engine/board.py
+++ kate/engine/board.py
@@ -12,47 +12,77 @@ class cBoard:
         '8' : 7,
     }
 
-    def __init__(self, PIECES):
-        self.PIECES = PIECES
-        self.fields = [ [PIECES['wRk'], PIECES['wKn'], PIECES['wBp'], PIECES['wQu'], PIECES['wKg'], PIECES['wBp'], PIECES['wKn'], PIECES['wRk']],
-                        [PIECES['wPw'], PIECES['wPw'], PIECES['wPw'], PIECES['wPw'], PIECES['wPw'], PIECES['wPw'], PIECES['wPw'], PIECES['wPw']],
-                        [PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk']],
-                        [PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk']],
-                        [PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk']],
-                        [PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk'], PIECES['blk']],
-                        [PIECES['bPw'], PIECES['bPw'], PIECES['bPw'], PIECES['bPw'], PIECES['bPw'], PIECES['bPw'], PIECES['bPw'], PIECES['bPw']],
-                        [PIECES['bRk'], PIECES['bKn'], PIECES['bBp'], PIECES['bQu'], PIECES['bKg'], PIECES['bBp'], PIECES['bKn'], PIECES['bRk']] ]
+    PIECES = {
+            'blk' : 0,
+            'wKg' : 1,
+            'wPw' : 2,
+            'wRk' : 3,
+            'wKn' : 4,
+            'wBp' : 5,
+            'wQu' : 6,
+            'bKg' : 9,
+            'bPw' : 10,
+            'bRk' : 11,
+            'bKn' : 12,
+            'bBp' : 13,
+            'bQu' : 14 
+        }
+
+        BLK = self.PIECES['blk']
+        wRK = self.PIECES['wRk']
+        wKN = self.PIECES['wKn']
+        wBP = self.PIECES['wBp']
+        wQU = self.PIECES['wQu']
+        wKG = self.PIECES['wKg']
+        wPW = self.PIECES['wPw']
+        bRK = self.PIECES['bRk']
+        bKN = self.PIECES['bKn']
+        bBP = self.PIECES['bBp']
+        bQU = self.PIECES['bQu']
+        bKG = self.PIECES['bKg']
+        bPW = self.PIECES['bPw']
+
+    def __init__(self):
+
+        self.fields = [ [wRK, wKN, wBP, wQU, wKG, wBP, wKN, wRK],
+                        [wPW, wPW, wPW, wPW, wPW, wPW, wPW, wPW],
+                        [BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK],
+                        [BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK],
+                        [BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK],
+                        [BLK, BLK, BLK, BLK, BLK, BLK, BLK, BLK],
+                        [bPW, bPW, bPW, bPW, bPW, bPW, bPW, bPW],
+                        [bRK, bKN, bBP, bQU, bKG, bBP, bKN, bRK] ]
 
     def set_to_base(self):
-        self.fields[self.COORD['1']][self.COORD['1']] = self.PIECES['wRk']
-        self.fields[self.COORD['1']][self.COORD['2']] = self.PIECES['wKn']
-        self.fields[self.COORD['1']][self.COORD['3']] = self.PIECES['wBp']
-        self.fields[self.COORD['1']][self.COORD['4']] = self.PIECES['wQu']
-        self.fields[self.COORD['1']][self.COORD['5']] = self.PIECES['wKg']
-        self.fields[self.COORD['1']][self.COORD['6']] = self.PIECES['wBp']
-        self.fields[self.COORD['1']][self.COORD['7']] = self.PIECES['wKn']
-        self.fields[self.COORD['1']][self.COORD['8']] = self.PIECES['wRk']
+        self.fields[self.COORD['1']][self.COORD['1']] = self.wRK
+        self.fields[self.COORD['1']][self.COORD['2']] = self.wKN
+        self.fields[self.COORD['1']][self.COORD['3']] = self.wBP
+        self.fields[self.COORD['1']][self.COORD['4']] = self.wQU
+        self.fields[self.COORD['1']][self.COORD['5']] = self.wKG
+        self.fields[self.COORD['1']][self.COORD['6']] = self.wBP
+        self.fields[self.COORD['1']][self.COORD['7']] = self.wKN
+        self.fields[self.COORD['1']][self.COORD['8']] = self.wRK
 
         for y in range (self.COORD['2'], (self.COORD['2'] + 1), 1):
             for x in range (self.COORD['1'], (self.COORD['8'] + 1), 1):
-                self.fields[y][x] = self.PIECES['wPw']
+                self.fields[y][x] = self.wPW
 
         for y in range (self.COORD['3'], (self.COORD['6'] + 1), 1):
             for x in range (self.COORD['1'], (self.COORD['8'] + 1), 1):
-                self.fields[y][x] = self.PIECES['blk']
+                self.fields[y][x] = self.BLK
 
         for y in range (self.COORD['7'], (self.COORD['7'] + 1), 1):
             for x in range (self.COORD['1'], (self.COORD['8'] + 1), 1):
-                self.fields[y][x] = self.PIECES['bPw']
+                self.fields[y][x] = self.bPW
 
-        self.fields[self.COORD['8']][self.COORD['1']] = self.PIECES['bRk']
-        self.fields[self.COORD['8']][self.COORD['2']] = self.PIECES['bKn']
-        self.fields[self.COORD['8']][self.COORD['3']] = self.PIECES['bBp']
-        self.fields[self.COORD['8']][self.COORD['4']] = self.PIECES['bQu']
-        self.fields[self.COORD['8']][self.COORD['5']] = self.PIECES['bKg']
-        self.fields[self.COORD['8']][self.COORD['6']] = self.PIECES['bBp']
-        self.fields[self.COORD['8']][self.COORD['7']] = self.PIECES['bKn']
-        self.fields[self.COORD['8']][self.COORD['8']] = self.PIECES['bRk']
+        self.fields[self.COORD['8']][self.COORD['1']] = self.bRK
+        self.fields[self.COORD['8']][self.COORD['2']] = self.bKN
+        self.fields[self.COORD['8']][self.COORD['3']] = self.bBP
+        self.fields[self.COORD['8']][self.COORD['4']] = self.bQU
+        self.fields[self.COORD['8']][self.COORD['5']] = self.bKG
+        self.fields[self.COORD['8']][self.COORD['6']] = self.bBP
+        self.fields[self.COORD['8']][self.COORD['7']] = self.bKN
+        self.fields[self.COORD['8']][self.COORD['8']] = self.bRK
 
     def writefield(self, x, y, value):
         self.fields[y][x] = value
@@ -65,7 +95,7 @@ class cBoard:
         y = srcy + stepy
         while(x >= self.COORD['1'] and x <= self.COORD['8'] and y >= self.COORD['1'] and y <= self.COORD['8']):
             field = self.readfield(x, y)
-            if(field != self.PIECES['blk']):
+            if(field != self.BLK):
                 return x, y
             x += stepx
             y += stepy
