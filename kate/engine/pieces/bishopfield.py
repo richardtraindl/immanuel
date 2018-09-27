1a61e5e6301604b853e4ff18c405f317a91ff30e
Richard Traindl <richardtraindl@gmail.com>
2018-09-27 11:16:49 +0200

    Update bishopfield.py

--- kate/engine/pieces/bishopfield.py
+++ kate/engine/pieces/bishopfield.py
@@ -1,9 +1,10 @@
+from .. values import *
 from .piecefield import cPieceField
 
 
 class cBishopField(cPieceField):
     def __init__(self, match, fieldx, fieldy):
-        super().__init__(match, fieldx, fieldy, [match.PIECES['wBp'], match.PIECES['wQu']], [match.PIECES['bBp'], match.PIECES['bQu']], [[1, 1], [-1, -1], [-1, 1], [1, -1]])
+        super().__init__(match, fieldx, fieldy, PIECES['wBp'], PIECES['wQu']], [PIECES['bBp'], PIECES['bQu']], [[1, 1], [-1, -1], [-1, 1], [1, -1]])
 
 # class end
 
