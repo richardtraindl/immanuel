8b33c18f05926bd930d56b058171ad803ad417a1
Richard Traindl <richardtraindl@gmail.com>
2018-09-27 11:27:22 +0200

    Update kingfield.py

--- kate/engine/pieces/kingfield.py
+++ kate/engine/pieces/kingfield.py
@@ -1,10 +1,11 @@
+from .. values import *
 from .piecefield import cPieceField
 from .piece import cTouch
 
 
 class cKingField(cPieceField):
     def __init__(self, match, fieldx, fieldy):
-        super().__init__(match, fieldx, fieldy, [match.PIECES['wKg']], [match.PIECES['bKg']], [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]])
+        super().__init__(match, fieldx, fieldy, [PIECES['wKg']], [PIECES['bKg']], [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]])
 
     def is_field_touched(self, color):
         for step in self.STEPS:
@@ -12,9 +13,9 @@ class cKingField(cPieceField):
             y1 = self.fieldy + step[1]
             if(self.match.is_inbounds(x1, y1)):
                 piece = self.match.readfield(x1, y1)
-                if(color == self.match.COLORS['white'] and piece == self.white_faces[0]):
+                if(color == COLORS['white'] and piece == self.white_faces[0]):
                     return True
-                elif(color == self.match.COLORS['black'] and piece == self.black_faces[0]):
+                elif(color == COLORS['black'] and piece == self.black_faces[0]):
                     return True
         return False
 
@@ -37,9 +38,9 @@ class cKingField(cPieceField):
             y1 = self.fieldy + step[1]
             if(self.match.is_inbounds(x1, y1)):
                 piece = self.match.readfield(x1, y1)
-                if(color == self.match.COLORS['white'] and piece == self.white_faces[0]):
+                if(color == COLORS['white'] and piece == self.white_faces[0]):
                     touches.append(cTouch(piece, x1, y1))
-                elif(color == self.match.COLORS['black'] and piece == self.black_faces[0]):
+                elif(color == COLORS['black'] and piece == self.black_faces[0]):
                     touches.append(cTouch(piece, x1, y1))
         return touches
 
@@ -51,11 +52,11 @@ class cKingField(cPieceField):
             if(self.match.is_inbounds(x1, y1)):
                 piece = self.match.readfield(x1, y1)
 
-                if(piece == self.match.PIECES['blk']):
+                if(piece == PIECES['blk']):
                     continue
 
                 if(self.match.color_of_piece(piece) == color):
-                    if(piece == self.match.PIECES['wKg'] or piece == self.match.PIECES['bKg'] or 
+                    if(piece == PIECES['wKg'] or piece == PIECES['bKg'] or 
                        self.match.is_field_touched(color, x1, y1, 1) == False):
                         count += 1
         return count
