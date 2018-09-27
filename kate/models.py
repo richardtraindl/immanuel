2b45719515e494b57460c92475f190d17724ce98
Richard Traindl <richardtraindl@gmail.com>
2018-09-27 12:47:05 +0200

    Update models.py

--- kate/models.py
+++ kate/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from kate.engine.values import *
 from kate.engine.match import *
 from kate.engine.move import *
 from kate.engine import helper
@@ -32,14 +33,14 @@ class Match(models.Model):
 
     def writefield(self, x, y, value):
         idx = y*32 + x*4
-        str_value = helper.reverse_lookup(cMatch.PIECES, value)
+        str_value = helper.reverse_lookup(PIECES, value)
         self.board = self.board[:idx] + str_value + self.board[(idx+3):]
 
 
     def readfield(self, x, y):
         idx = y*32 + x*4
         str_value = self.board[idx:idx+3]
-        return cMatch.PIECES[str_value]
+        return PIECES[str_value]
 
 
     #def is_immanuel(self):
@@ -63,8 +64,8 @@ class Move(models.Model):
     dsty = models.PositiveSmallIntegerField(null=False)
     e_p_fieldx = models.PositiveSmallIntegerField(null=True)
     e_p_fieldy = models.PositiveSmallIntegerField(null=True)
-    captured_piece = models.PositiveSmallIntegerField(null=False, default=cMatch.PIECES['blk'])
-    prom_piece = models.PositiveSmallIntegerField(null=False, default=cMatch.PIECES['blk'])
+    captured_piece = models.PositiveSmallIntegerField(null=False, default=PIECES['blk'])
+    prom_piece = models.PositiveSmallIntegerField(null=False, default=PIECES['blk'])
     fifty_moves_count = models.SmallIntegerField(null=False)
 
 
