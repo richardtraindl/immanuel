a0328d5879e8db9402158618c750fb1d2080afd6
richardtraindl <richardtraindl@gmail.com>
2019-04-08 21:25:21 +0200

    remove whitenoise

--- immanuel/settings.py
+++ immanuel/settings.py
@@ -19,7 +19,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  
+    #'whitenoise.middleware.WhiteNoiseMiddleware',  
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -29,7 +29,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'immanuel.urls'
 
--- immanuel/wsgi.py
+++ immanuel/wsgi.py
@@ -1,15 +1,7 @@
-"""import os
+import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "immanuel.settings")
 
-application = get_wsgi_application()"""
-
-from django.core.wsgi import get_wsgi_application
-
-from whitenoise import WhiteNoise
-
 application = get_wsgi_application()
-application = WhiteNoise(application, root='static') # '/path/to/static/files'
-#application.add_files('/path/to/more/static/files', prefix='more-files/')
