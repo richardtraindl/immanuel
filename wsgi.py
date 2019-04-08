66ac5b71af9424869202ec0ff21629a1bd4440c8
richardtraindl <richardtraindl@gmail.com>
2019-04-08 21:28:16 +0200

    remove whitenoise

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,18 @@
-import os
+"""import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "immanuel.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)"""
 
 
 
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "immanuel.settings")
+
+application = get_wsgi_application()
