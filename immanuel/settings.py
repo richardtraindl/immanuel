5542ac43bf1b34ba84be76d3cac5c23108201ffc
richardtraindl <richardtraindl@gmail.com>
2019-04-08 20:53:37 +0200

    fix

--- immanuel/settings.py
+++ immanuel/settings.py
@@ -29,6 +29,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware', 
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'immanuel.urls'
 
 TEMPLATES = [
